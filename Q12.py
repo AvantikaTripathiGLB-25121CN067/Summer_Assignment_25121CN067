d0996bfd0f07db46e98fa48e5c6354665e698eb6
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-04 15:01:11 +0530

    Day3 Q012

--- Q12.py
+++ Q12.py
@@ -4,10 +4,13 @@ if (a>b):
     n=a
 else:
     n=b
-    while True:
-        if (n%a==0) and (n%b==0):
-            lcm=n
-            break
+
+while True:
+    if(n%a==0)and(n%b==0):
+        lcm=n
+        break
         n+=1
+    
+    
        
 print(lcm)
