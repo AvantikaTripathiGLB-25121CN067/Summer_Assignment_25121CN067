36dbb6374ead6b5153abc151560999cd3ace0989
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-09 18:53:51 +0530

    Create Q45.py

---
+++ Day12/Q45.py
@@ -0,0 +1,13 @@
+def palindrome(n):
+    if(n<0):
+        return'no result'
+    else:
+        original=n
+        reversed=0
+        while(n>0):
+            d=n%10
+            reversed=reversed*10+d
+            n//=10
+        return original==reversed
+n=int(input('n'))
+print(palindrome(n))
