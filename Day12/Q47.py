84f29cbc2e7f22f0570e872005596d65de66ff74
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-09 18:54:06 +0530

    Create Q47.py

---
+++ Day12/Q47.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if(n<0):
+        return'no result'
+    else:
+        a=0
+        b=1
+        for i in range(n):
+            a,b=b,a+b
+        return a
+            
+n=int(input('n'))
+print(fibonacci(n))
