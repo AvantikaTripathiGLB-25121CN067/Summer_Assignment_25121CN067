ef8c94558bfe24d3af80c0379a78fc8ae33f7948
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-11 14:11:45 +0530

    Update Day4 Q015.py

--- Day4 Q015.py
+++ Day4 Q015.py
@@ -1,5 +1,14 @@
-digits='370'
-power=len(digits)
-
-total =sum(int(digit)**power for digit in digits)
-print(total)
+n=int(input("n:"))
+if n<0:
+    print("no result")
+else:
+    original=n
+    sum=0
+    while n>0:
+        digit=n%10
+        sum+=digit**3
+        n//=10
+    if sum==original:
+        print("Armstrong number")
+    else:
+        print("Not an Armstrong number")
