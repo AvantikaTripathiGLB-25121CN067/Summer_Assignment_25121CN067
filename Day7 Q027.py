b99ec5e2ac1883ae4b50b954dd1f996174a24996
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-12 19:07:38 +0530

    Delete Day7 Q027.py

--- Day7 Q027.py
+++
@@ -1,9 +0,0 @@
-def sum_of_digits(n):
-    n=abs(n)
-    if n<10:
-        return n
-    last_digit=n%10
-    remaining_digits=n//10
-    return last_digit+sum_of_digits(remaining_digits)
-n=int(input("enter the number:"))
-print(f"the sum of the digits of {n} is: {sum_of_digits(n)}")
