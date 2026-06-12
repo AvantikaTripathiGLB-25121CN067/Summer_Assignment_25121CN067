6549c6bf8a0d09870e504a721e18c3adc4ad6d49
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-12 19:04:20 +0530

    Create Day10 Q38.py

---
+++ Day10/Day10 Q38.py
@@ -0,0 +1,16 @@
+i=5
+k=9
+while i>=1:
+    b=5
+    while b>i:
+        print(' ',end='')
+        b-=1
+    j=1
+    while j<=k:
+         
+        print('*',end='')
+        j+=1
+        
+    k-=2
+    print()
+    i-=1
