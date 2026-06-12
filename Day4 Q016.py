149f1023291ef759c274f137ae94f893c4fbe1d2
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-12 13:09:57 +0530

    Delete Day4 Q016.py

--- Day4 Q016.py
+++
@@ -1,9 +0,0 @@
-lower_range=300
-upper_range=400
-print('Armstrong number between lower and upper+1')
-for num in range(lower_range,upper_range+1):
-    digits=str(num)
-    power=len(digits)
-    total=sum(int(digit)**power for digit in digits)
-    if total==num:
-        print(num)
