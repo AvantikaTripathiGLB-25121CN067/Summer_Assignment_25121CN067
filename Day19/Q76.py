76fa36c77b906fcba93291dc6580eb92299fb4a0
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-19 13:37:01 +0530

    Create Q76.py

---
+++ Day19/Q76.py
@@ -0,0 +1,13 @@
+rows=int(input("enter the number of rows:"))
+cols=int(input("enter the number of columns:"))
+
+print("enter the elements of matrix:")
+matrix=[[int(input(f"[{i}][{j}]:")) for j in range(cols) ] for i in range(rows)]
+
+diag_sum=0
+for i in range(rows):
+    diag_sum +=matrix[i][i]
+
+for r in matrix:
+    print(r)
+print(diag_sum)
