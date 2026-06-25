e19374e2248dcc20e5ab7e83f77ccdb6cd7a0e30
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-25 20:30:29 +0530

    Create Q97.py

---
+++ Day25/Q97.py
@@ -0,0 +1,19 @@
+arr1=input("enter the values:").split()
+arr2=input("enter the values:").split()
+merged=[]
+
+i=0
+j=0
+
+while i <len(arr1) and j<len(arr2):
+    if arr1[i]<arr2[j]:
+        merged.append(arr1[i])
+        i+=1
+    else:
+        merged.append(arr2[j])
+        j+=1
+
+merged.extend(arr1[i:])
+merged.extend(arr2[j:])
+
+print("merged array:",merged)
