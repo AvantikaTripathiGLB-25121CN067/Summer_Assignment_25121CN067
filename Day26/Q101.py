c778eef9323de2b8168de9bb312fe14e4cab1cef
AvantikaTripathiGLB-25121CN067 <tripathiavantika89@gmail.com>
2026-06-26 16:03:16 +0530

    Create Q101.py

---
+++ Day26/Q101.py
@@ -0,0 +1,19 @@
+class NumberGuessingGame:
+    def __init__(self,secret_number):
+        self.secret_number=secret_number
+        self.attempts=0
+
+    def make_guess(self,guess):
+        self.attempts+=1
+        if guess<self.secret_number:
+            return "Too Low"
+        elif guess>self.secret_number:
+            return "Too High"
+        else:
+            return "correct! This is the secret number."
+        
+game=NumberGuessingGame(secret_number=40)
+print(game.make_guess(25))
+print(game.make_guess(45))
+            
+
